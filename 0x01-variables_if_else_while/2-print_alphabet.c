2409a101be950d09cd3d196b828a012ab8b06481
Waheed Khaled <waheedkhaled95@gmail.com>
2023-03-18 00:18:18 +0000

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int decimal = 97;
+	/* Declerations */
+	/* Statements */
+	while (decimal < 123)
+	{
+		putchar(decimal);
+		++decimal;
+	}
+	putchar(10);
+	return (0);
+}

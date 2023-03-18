96905b1b0438756d61d841d362c135771222b012
Waheed Khaled <waheedkhaled95@gmail.com>
2023-03-18 00:41:51 +0000

    Tast 5 done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int decimal = 48;
+	/* Declerations */
+	/* Statements */
+	while (decimal < 58)
+	{
+		putchar(decimal);
+		++decimal;
+	}
+	putchar(10);
+	return (0);
+}

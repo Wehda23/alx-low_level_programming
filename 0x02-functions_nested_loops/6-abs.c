32e0aeb2655f0b78befe528bb3b56801dbd421f6
Waheed Khaled <waheedkhaled95@gmail.com>
2023-03-22 01:33:34 +0000

    task 7 done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - compares the absolute value of an integer
+ * @r: An integer input
+ * Description: This function returns absolute value of a number
+ * Return: Absolute value of number r
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}

abaee363a62a5d8fe1fbf51adbe328136151dc72
Waheed Khaled <waheedkhaled95@gmail.com>
2023-03-22 01:01:06 +0000

    third task done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints alphabets x10 times
+ *
+ * Return: Always void (Success)
+ */
+void print_alphabet_x10(void);
+{
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		print_alphabet();
+	}
+}

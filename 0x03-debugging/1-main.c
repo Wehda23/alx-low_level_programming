854540799df1abd5ae9cb010f024e0fc0c79682f
Waheed Khaled <waheedkhaled95@gmail.com>
2023-03-23 23:40:39 +0000

    hot fix

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - causes an infinit loop
+ * main - causes an infinite loop
  * Return: 0
  */
 
@@ -18,7 +18,6 @@ int main(void)
 	*	putchar(i);
 	*}
 	*/
-
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);

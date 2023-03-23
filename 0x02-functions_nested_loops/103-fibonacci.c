8ded46b7471bdb4e6ac5987df1981c64d915bbcd
Waheed Khaled <waheedkhaled95@gmail.com>
2023-03-23 02:02:33 +0000

    fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,18 +9,18 @@
 int main(void)
 {
 	int i;
-	long int list[50];
+	long int list[50], sum = 2;
 
 	list[0] = 1;
 	list[1] = 2;
-	printf("%ld, %ld, ", list[0], list[1]);
 
-	for (i = 2; i < 4000000; i++)
+	for (i = 2; i < 50; i++)
 	{
 		list[i] = list[i - 1] + list[i - 2];
-		if (list[i] % 2 == 0)
-			printf("%ld\n", list[i]);
+		if ((list[i] % 2 == 0) && list[i] < 4000000)
+			sum += list[i];
 	}
+	printf("%ld\n", sum);
 
 	return (0);
 }

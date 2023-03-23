5eb93ff6a9a1595a89a6aee54312af05b88a4fc8
Waheed Khaled <waheedkhaled95@gmail.com>
2023-03-23 00:36:51 +0000

    hot fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,11 +13,45 @@ void print_times_table(int n)
 	{
 	for (; n < 15; n++)
 	{
+		_putchar('0');
 		for (i = 0; i <= n; i++)
 		{
 			_putchar(n * i);
 		}
+		_putchar('\n');
 	}
 	}
 
 }
+
+/**
+ * putformat - formatted characters to output
+ * @n: number to format
+ * Return: nothing
+ */
+
+void putformat(int n)
+{
+	_putchar(',');
+	_putchar(' ');
+
+	if (n <= 9)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n + '0');
+	}
+	else if (n > 9 && n <= 99)
+	{
+		_putchar(' ');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else
+	{
+		_putchar(n / 100 + '0');
+		_putchar(n / 10 % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+}
+

6d4b803730a7fc83becb75961fb3352f371366b6
Waheed Khaled <waheedkhaled95@gmail.com>
2023-04-03 23:59:39 +0000

    Removed trailing whitespace

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _memcpy - copies memory area.
- * @dest: input string 
- * @src: input string
- * @n: unsigned input integer
- * Return: pointer dest
- */
+* _memcpy - copies memory area.
+* @dest: input string
+* @src: input string
+* @n: unsigned input integer
+* Return: pointer dest
+*/
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int index = 0;
+	unsigned int index = 0;
 
-    while (index < n)
-    {
-        *(dest + index) = *(src + index);
-        index++;
-    }
-    return (dest);
+	while (index < n)
+	{
+		*(dest + index) = *(src + index);
+		index++;
+	}
+	return (dest);
 }

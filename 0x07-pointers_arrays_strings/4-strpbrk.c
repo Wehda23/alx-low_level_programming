08db0fa8392e2fbf5096d45e9f45e8b1a27129b0
Waheed Khaled <waheedkhaled95@gmail.com>
2023-04-04 01:04:50 +0000

    Betty hot fix

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,30 +1,29 @@
 #include "main.h"
 
 /**
- * _strpbrk - Locates the first occurance in the string s of any of the bytes in 
- *            string accept
+ * _strpbrk - Locates the first occurance in the string s of any of the bytes
  * @s: string input
  * @accept: string input
- * Return: pointer to the byte in s that matches one of the bytes in accept or Null
+ * Return: pointer to the byte in s that matches one of the bytes or Null
  */
 
 char *_strpbrk(char *s, char *accept)
 {
 
-    int index, mainString;
+	int index, mainString;
 
-    while (*(s + index) != '\0')
-    {
-        while (*(accept + mainString) != '\0')
-        {
-            if (*(accept + mainString) == *(s + index))
-                return (s + index);
+	while (*(s + index) != '\0')
+	{
+		while (*(accept + mainString) != '\0')
+		{
+			if (*(accept + mainString) == *(s + index))
+				return (s + index);
 
-            mainString++;
-        }
-        mainString = 0;
-        index++;
-    }
+			mainString++;
+		}
+		mainString = 0;
+		index++;
+	}
 
-    return ('\0');
+	return ('\0');
 }

552c2bdf0b824e0c41d44c9f059e8ad16666635e
Waheed Khaled <waheedkhaled95@gmail.com>
2023-04-04 00:46:37 +0000

    Betty hot fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,34 +3,34 @@
 /**
  * _strspn - Gets the length of a prefix substring
  * @s: string input
- * @s: string input
+ * @accept: string input
  * Return: Number of bytes in the initial segement of s
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-    int index, subIndex;
-    unsigned int bytes;
+	int index, subIndex;
+	unsigned int bytes;
 
-    while (*(s + index) != '\0')
-    {
-        while (*(accept + subIndex)  != '\0')
-        {
-            if (*(s + index) == *(accept + subIndex))
-            {
-                bytes++;
-                break;
-            }
-            else if (accept[subIndex + 1] == '\0')
-                return (bytes);
+	while (*(s + index) != '\0')
+	{
+		while (*(accept + subIndex)  != '\0')
+		{
+			if (*(s + index) == *(accept + subIndex))
+			{
+				bytes++;
+				break;
+			}
+			else if (accept[subIndex + 1] == '\0')
+				return (bytes);
 
-            subIndex++;
-        }
-        subIndex = 0;
-        index++;
-    }
+			subIndex++;
+		}
+		subIndex = 0;
+		index++;
+	}
 
 
 
-    return (bytes);
+	return (bytes);
 }

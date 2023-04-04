37fd400d641a4b157a96ae9471af63adb821a07b
Waheed Khaled <waheedkhaled95@gmail.com>
2023-04-04 00:21:22 +0000

    Hot fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,10 +13,10 @@ char *_strchr(char *s, char c)
 	int index = 0;
 
 	for (index = 0; s[index] >= '\0' ; index++)
-    {
-        if (s[index] == c)
-            return (s + index);
-    }
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
 
 	return ('\0');
 }

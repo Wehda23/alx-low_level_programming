5c0806dce0d85bfebe1e9a99a313a22c61269662
Waheed Khaled <waheedkhaled95@gmail.com>
2023-04-06 00:03:41 +0000

    Factorial Betty edit

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,14 +9,14 @@
 
 int factorial(int n)
 {
-    
-    
-    if (n > 0)
-    {
-        return n * factorial(n - 1);
-    }
-    else if (n < 0)
-        return -1;
-    else
-        return 1;
+
+
+	if (n > 0)
+	{
+		return (n * factorial(n - 1));
+	}
+	else if (n < 0)
+		return (-1);
+	else
+		return (1);
 }

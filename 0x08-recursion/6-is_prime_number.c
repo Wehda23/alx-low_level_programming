7cc8378e10203c065a7582205f4b94a07441f34f
Waheed Khaled <waheedkhaled95@gmail.com>
2023-04-06 01:43:38 +0000

    Edit betty for prime file

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,22 +3,21 @@
 
 /**
  * check_prime - is a function that divides number and checks prime
- * 
- * @number: Input integer 
- * @divisable_number: input integer decrementing 
+ * @number: Input integer
+ * @divisable_number: input integer decrementing
  * Return: return 1 if the number is a prime number and 0 if not.
  */
 
 int check_prime(int number, int divisable_number)
 {
 
-    if(divisable_number == 1)
-        return (1);
-    
-    if(number % divisable_number == 0)
-        return (0);
+	if (divisable_number == 1)
+		return (1);
 
-    return (check_prime(number, divisable_number - 1));
+	if (number % divisable_number == 0)
+		return (0);
+
+	return (check_prime(number, divisable_number - 1));
 }
 
 /**
@@ -31,11 +30,11 @@ int check_prime(int number, int divisable_number)
 
 int is_prime_number(int n)
 {
-    if (n <= 1)
-        return (0);
+	if (n <= 1)
+		return (0);
 
-    if (n == 2 || n == 3)
-        return (1);
+	if (n == 2 || n == 3)
+		return (1);
 
-    return check_prime(n, n - 1);
+	return (check_prime(n, n - 1));
 }

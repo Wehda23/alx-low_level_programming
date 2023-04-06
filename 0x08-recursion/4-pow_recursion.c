79c822f036c60754d03808fc3f1eb27ecbe6b021
Waheed Khaled <waheedkhaled95@gmail.com>
2023-04-06 02:34:07 +0200

    Task 4 power done

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - powers up the number to a certain power
+ * @x: input integer as value
+ * @y: input integer as power
+ * Return: returns the value powered up
+ */
+
+int _pow_recursion(int x, int y)
+{
+    if (y < 0)
+        return (-1);
+    else if (y == 0)
+        return (1);
+    else
+        return (x * _pow_recursion(x, y - 1));
+}

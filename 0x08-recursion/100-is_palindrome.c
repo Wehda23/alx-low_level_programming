7f74bf57342097c23ed8a9429c971fc9f45852af
Waheed Khaled <waheedkhaled95@gmail.com>
2023-04-07 04:44:23 +0200

    palindrom

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * check_palindrom - check for palindrom
+ * @string: string input
+ * @start: integer input
+ * @end: integer input
+ * Return: returns 1 if palindrom otherwise 0
+ */
+int check_palindrom(char *string, int start, int end)
+{
+    
+    if(*string == '\0' || start >= end)
+    {
+        return (1);
+    }
+    else if(string[start] != string[end])
+    {
+        return (0);
+    }
+    {      
+        return (check_palindrom(string, start + 1, end - 1));
+    }
+    
+
+
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrom.
+ * 
+ * @s: String input 
+ * Return: retuns 1 if string is palindrom otherwise 0 
+ */
+
+int is_palindrome(char *s)
+{
+    int len;
+
+    while(*(s + len))
+    {
+        len = len + 1;
+    }
+    len -= 1;
+
+    return (check_palindrom(s, 0, len));
+}

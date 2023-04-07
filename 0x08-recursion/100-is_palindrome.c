831d0140f2d549c3655d09f8c5a0495b09606df0
Waheed Khaled <waheedkhaled95@gmail.com>
2023-04-07 05:29:06 +0200

    update

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,44 @@
 #include "main.h"
 
+
+
+
+
+/**
+ * string_counter - helper function to get length of a string
+ * 
+ * @string: string input.
+ * @length: integer input.
+ * Return: Length of a string
+ */
+int string_counter(char *string, int length)
+{
+    if (*string)
+    {
+        return (string_counter(string + 1, length + 1));
+    }
+    else
+        return (length);
+
+}
+
+/**
+ * _strlen_recursion - Get the lenght of a string.
+ * @s: string input
+ * Return: length of a string.
+ */
+
+
+
+int _strlen_recursion(char *s)
+{
+
+    return (string_counter(s, 0));
+
+}
+
+
+
 /**
  * check_palindrom - check for palindrom
  * @string: string input
@@ -33,7 +72,7 @@ int is_palindrome(char *s)
 {
 	int len;
 
-	len = _strlen_recursion(s);
+	len = _strlen_recursion(s) - 1;
 
 	return (check_palindrom(s, 0, len));
 }

b0a7980f40016f41c3635d851a13587278910f7c
Waheed Khaled <waheedkhaled95@gmail.com>
2023-04-16 23:54:15 +0200

    last task done

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * array_range - create array of integer values
+ * @min: minimum number
+ * @max: maximum number
+ * Return: a newly created array
+ */
+int *array_range(int min, int max)
+{
+	unsigned int width;
+    int *range;
+
+	if (max < min)
+		return (NULL);
+
+	width = max - min;
+
+	range = malloc(sizeof(int) * (width + 1));
+	if (!range)
+		return (NULL);
+
+	do {
+		*range++ = min++;
+	} while (min <= max);
+
+	return (range - width - 1);
+}

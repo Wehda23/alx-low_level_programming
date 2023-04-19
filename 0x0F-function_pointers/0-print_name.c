2b457fa2782d064b8e392eac177722a3c1d89c39
Waheed Khaled <waheedkhaled95@gmail.com>
2023-04-20 01:27:40 +0200

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,15 @@
 #include "function_pointers.h"
-
+#include <stdlib.h>
 /**
  * print_name - print name function
  * @name: string input
  * @f: function to call the print name function.
  * Return: Void.
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (f)
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
 }
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * array_iterator - iterates through an array
+ * @array: is the array
+ * @size: size of the array
+ * @action: do some thing
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL)
+		return;
+	if (action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}

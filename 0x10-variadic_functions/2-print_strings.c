e7038a4d9090f2c16af21cf2599a0c20683a032a
Waheed Khaled <waheedkhaled95@gmail.com>
2023-04-21 02:15:01 +0000

    Edited to pass betty test

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -5,25 +5,26 @@
  * @separator: a character input as in form of separator.
  * @n: is an integer input, represents number of integers passed.
  * @...: multiple arguements passed into the function.
- * Return: Void. 
+ * Return: Void.
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-    int i = n;
-    char *s;
-    va_list ap;
+	int i = n;
+	char *s;
+	va_list ap;
 
-    if (n)
-    {
-        va_start(ap, n);
+	if (n)
+	{
+		va_start(ap, n);
 
-        while (i--)
-        {
-            printf("%s%s", (s = va_arg(ap, char *)) ? s : "(nil)", i ? (separator ? separator : "") : "\n");
-        }
+		while (i--)
+		{
+			printf("%s%s", (s = va_arg(ap, char *)) ? s : "(nil)",
+					i ? (separator ? separator : "") : "\n");
+		}
 
-        va_end(ap);
-    }
-    else
-        printf("\n");
+		va_end(ap);
+	}
+	else
+		printf("\n");
 }

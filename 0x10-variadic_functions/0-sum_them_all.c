f3ab832c149687c50d2c3bf37a7eee3cf5c9a451
Waheed Khaled <waheedkhaled95@gmail.com>
2023-04-21 03:40:40 +0200

    2 tasks done

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - is a function that returns the sum of all it's arguments.
+ * @n: integer input.
+ * @...: argurments passed.
+ * Return: an integer return which is sum of all inputs gathered.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+    va_list ap;
+    int sum, i = n;
+
+    if (n == 0)
+            return (0);
+
+    va_start(ap, n);
+
+    while (i--)
+    {
+        sum += va_arg(ap, int);
+    }
+
+    va_end(ap);
+    
+    return (sum);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers -  prints numbers, followed by a new line.
+ * @separator: a character input as in form of separator.
+ * @n: is an integer input, represents number of integers passed.
+ * @...: multiple arguements passed into the function.
+ * Return: Void.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+    va_list ap;
+    int i = n;
+    
+    if (!n)
+    {
+        printf("\n");
+    }
+    else
+    {
+        va_start(ap, n);
+        while (i--)
+                printf("%d%s", va_arg(ap,int), i ? (separator ? separator : "") : "\n");
+        
+        va_end(ap);
+    }
+
+    
+}

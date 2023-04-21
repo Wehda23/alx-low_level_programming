fc5a35177ff451d73114c41f2da55c8abe6459d0
Waheed Khaled <waheedkhaled95@gmail.com>
2023-04-21 04:58:46 +0200

    e

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,91 @@
+#include "variadic_functions.h"
+
+
+/**
+ * format_char - format character prints character.
+ * @separator: String separator.
+ * @ap: argument pointer.
+ * Return: Void.
+ */
+void format_char(char *separator, va_list ap)
+{
+    printf("%s%c", separator, va_arg(ap, int));
+}
+
+/**
+ * format_int - format character prints integers.
+ * @separator: String separator.
+ * @ap: argument pointer.
+ * Return: Void.
+ */
+void format_int(char *separator, va_list ap)
+{
+    printf("%s%d", separator, va_arg(ap, int));
+}
+
+/**
+ * format_char - format character prints float
+ * @separator: String separator.
+ * @ap: argument pointer.
+ * Return: Void.
+ */
+void format_float(char *separator, va_list ap)
+{
+    printf("%s%f", separator, va_arg(ap, double));
+}
+
+/**
+ * format_char - format character prints a string
+ * @separator: String separator.
+ * @ap: argument pointer.
+ * Return: Void.
+ */
+void format_string(char *separator, va_list ap)
+{
+    char *s = va_arg(ap, char *);
+    
+    switch ((int)(!s))
+            case 1:
+                    s = "(nil)";
+    
+    printf("%s%s", separator, s);
+}
+
+/**
+ * print_all - prints anything.
+ * @format: list of types of arguments passed to the function.
+ * @...: arguements passed into the function.
+ * Return: Void.
+ */
+
+void print_all(const char * const format, ...)
+{
+    int i, j;
+    char *separator = "";
+    va_list ap;
+    token_t tokens[] = {
+        {"c", format_char},
+        {"i", format_int},
+        {"f", format_float},
+        {"s", format_string},
+        {NULL, NULL}
+    };
+
+    va_start(ap, format);
+    while (format && format[i])
+    {
+        j = 0;
+        while (tokens[j].token)
+        {
+            if (format[i] == tokens[j].token[0])
+            {
+                tokens[j].f(separator, ap);
+                separator = ", ";
+            }
+            j++;
+        }
+        i++;
+    }
+    printf("\n");
+    va_end(ap);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,17 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ *  struct token - Struct token.
+ *  @token: string input as format token.
+ *  @f: function concerned with that token.
+ */
+typedef struct token
+{
+    char *token;
+    void (*f)(char *, va_list);
+} token_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);

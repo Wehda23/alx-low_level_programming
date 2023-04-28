de03ca44b394384b74fac801b87972ccef2fc8ad
Waheed Khaled <waheedkhaled95@gmail.com>
2023-04-28 00:09:28 +0000

    Adjusted file to pass betty style

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,17 +4,17 @@
 /**
  * list_len - function that returns number of elements in Linked list.
  * @h: pointer to a node in a linked list.
- * Return: size_t which represents the number of elements in linked list. 
+ * Return: size_t which represents the number of elements in linked list.
  */
 size_t list_len(const list_t *h)
 {
-    size_t length = 0;
+	size_t length = 0;
 
-    while (h)
-    {
-        h = h->next;
-        length++;
-    }
-    
-    return (length);
+	while (h)
+	{
+		h = h->next;
+		length++;
+	}
+
+	return (length);
 }

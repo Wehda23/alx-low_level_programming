2b089c0cd575770dd8c66a0299ae03159fa3f2d8
Waheed Khaled <waheedkhaled95@gmail.com>
2023-04-28 01:25:21 +0000

    Add node adjusted for betty style pass test

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,33 +1,38 @@
 #include "lists.h"
 
-
+/**
+ * add_node - function that adds a node to start.
+ * @head: is a pointer to a pointer of a node.
+ * @str: Pointer to a string character.
+ * Return: Memory addess of the node.
+ */
 
 list_t *add_node(list_t **head, const char *str)
 {
 
-    list_t *new;
-    char *s;
+	list_t *new;
+	char *s;
 
-    if (!str)
-        return (NULL);
-    new = malloc(sizeof(list_t));
+	if (!str)
+		return (NULL);
+	new = malloc(sizeof(list_t));
 
-    if (!new)
-        return (NULL);
-    
-    s = strdup(str);
-    
-    if (!s)
-    {
-        free(new);
-        return (NULL);
-    }
-    
-    new->str = strdup(str);
-    new->len = strlen(new->str);
-    new->next = *head;
+	if (!new)
+		return (NULL);
 
-    *head = new;
+	s = strdup(str);
 
-    return (*head);
+	if (!s)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->str = strdup(str);
+	new->len = strlen(new->str);
+	new->next = *head;
+
+	*head = new;
+
+	return (*head);
 }

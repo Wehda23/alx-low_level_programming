9a4ea5889e72b8d8881fb80eb503760861074646
Waheed Khaled <waheedkhaled95@gmail.com>
2023-05-03 00:30:07 +0000

    Edit to pass betty test

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the length of the nodes.
- * @h: is a pointer to node. 
- * Return: Length of a node. 
+ * listint_len - returns the length of the nodes
+ * @h: is a pointer to node.
+ * Return: Length of a node.
  */
 size_t listint_len(const listint_t *h)
 {
-    size_t count;
+	size_t count;
 
-    for (count = 0; h ; count++)
-        h = h->next;
+	for (count = 0; h ; count++)
+		h = h->next;
 
-    return (count);
+	return (count);
 }

a32f50b1544ff930f80da537db6599fd864eb65f
Waheed Khaled <waheedkhaled95@gmail.com>
2023-05-03 00:52:10 +0000

    Edit to pass betty test

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,25 +3,25 @@
 /**
  * add_nodeint - is a function that adds a node to the linked list.
  * @head: is a pointer to an pointer that saves address of a node.
- * @n: is an integer input. 
- * Return: Return the memory address of the node. 
+ * @n: is an integer input.
+ * Return: Return the memory address of the node.
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-    listint_t *clone;
+	listint_t *clone;
 
-    if (!n)
-        return (NULL);
+	if (!n)
+		return (NULL);
 
-    clone = malloc(sizeof(listint_t));
+	clone = malloc(sizeof(listint_t));
 
-    if (!clone)
-        return (NULL);
-    
-    clone->n = n;
-    clone->next = *head;
+	if (!clone)
+		return (NULL);
 
-    *head = clone;
-    
-    return (clone);
+	clone->n = n;
+	clone->next = *head;
+
+	*head = clone;
+
+	return (clone);
 }

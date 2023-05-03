9dd8133e8c929364292aaf25ff677cdb9ec14fb3
Waheed Khaled <waheedkhaled95@gmail.com>
2023-05-03 03:51:13 +0300

    Task 3 done

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - is a function that adds a node to the linked list.
+ * @head: is a pointer to an pointer that saves address of a node.
+ * @n: is an integer input. 
+ * Return: Return the memory address of the node. 
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+    listint_t *clone;
+
+    if (!n)
+        return (NULL);
+
+    clone = malloc(sizeof(listint_t));
+
+    if (!clone)
+        return (NULL);
+    
+    clone->n = n;
+    clone->next = *head;
+
+    *head = clone;
+    
+    return (clone);
+}

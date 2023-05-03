192c6099e27280d8c720c3cb5915ebe51cbe6413
Waheed Khaled <waheedkhaled95@gmail.com>
2023-05-03 00:20:57 +0000

    Betty

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * print_listint - is a function the prints n vairable of a node.
+ * @h: is a pointer to node.
+ * Return: lenght of the nodes.
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t count;

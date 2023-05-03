5e8ec13c246afe90b7b94df3b4650de6840fc74f
Waheed Khaled <waheedkhaled95@gmail.com>
2023-05-03 04:04:01 +0300

    Task done

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+
+/**
+ * add_nodeint_end - adds a node at end of the tail.
+ * @head: a pointer to a pointer to an address of a node. 
+ * @n: A const input integer.
+ * Return: Memory address of the node. 
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+
+    listint_t *tail, *clone;
+
+    tail = *head;
+
+    if (!n)
+        return (NULL);
+
+    clone = malloc(sizeof(listint_t));
+
+    if (!clone)
+        return (NULL);
+    
+    clone->n = n;
+	clone->next = NULL;
+
+	if (*head == NULL)
+		*head = clone;
+	else
+	{
+		tail = *head;
+		while (tail->next)
+			tail = tail->next;
+		tail->next = clone;
+	}
+
+    return (*head);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,26 +1,29 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef TEST_H
+#define TEST_H
 
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
  */
-typedef struct listint_s
+typedef struct list_s
 {
-    int n;
-    struct listint_s *next;
-} listint_t;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
-size_t print_listint(const listint_t *h);
-size_t listint_len(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+
 
 #endif

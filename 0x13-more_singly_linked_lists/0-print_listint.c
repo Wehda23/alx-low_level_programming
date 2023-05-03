22a7b257d46bb6523106cc47d7fef83677353c4a
Waheed Khaled <waheedkhaled95@gmail.com>
2023-05-03 00:19:59 +0000

    Edit for betty style pass test

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,18 +2,18 @@
 
 size_t print_listint(const listint_t *h)
 {
-    size_t count;
-    
-    for (count = 0; h ; count++)
-    {
-        if (h->n)
-        {
-            printf("%d\n", h->n);
-        }
-        h = h->next;
-    }
-        
-    
+	size_t count;
 
-    return (count);
+	for (count = 0; h ; count++)
+	{
+		if (h->n)
+		{
+			printf("%d\n", h->n);
+		}
+		h = h->next;
+	}
+
+
+
+	return (count);
 }

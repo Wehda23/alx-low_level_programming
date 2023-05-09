77c1094b75ab82ade7c9d6c0a05ae72910768dc1
Waheed Khaled <waheedkhaled95@gmail.com>
2023-05-09 03:49:31 +0300

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,28 +9,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int folder, length, w;
+	int fd, _write, i; /* i: is the len of the content */
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    folder = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
-    if (folder == -1)
-        return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[i])
+			i++;
 
-    if (text_content != NULL)
-    {
-
-            while (text_content[length++]);
-            
-            w = write(folder, text_content, length);
-
-            if (w == -1)
-                return (-1);
-    }
-
-    close(folder);
-
-    return (1);
+		_write = write(fd, text_content, i);
+		if (_write == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
 }

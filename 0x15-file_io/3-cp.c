913a97cda165a40ea17f45f7d30aa8e448d6b694
Waheed Khaled <waheedkhaled95@gmail.com>
2023-05-09 05:17:47 +0300

    adde last task

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: Number of arguements.
+ * @argv: list of argurments.
+ * Return: Always (0) success.
+ */
+
+int main(int argc, char *argv[])
+{
+	int source, new;
+	int r, close_source, close_new;
+	char buffer[1024];
+
+	if (argc > 3 || argc < 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	source = open(*(argv + 1), O_RDONLY);
+    
+	if (source == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", *(argv + 1));
+		exit(98);
+	}
+
+	new = open(*(argv + 2), O_TRUNC | O_CREAT | O_WRONLY, 0664);
+
+	while ((r = read(source, buffer, 1024)) > 0)
+	{
+		if (new == -1 || (write(new, buffer, r) != r))
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", *(argv + 2));
+			exit(99);
+		}
+	}
+
+	if (r < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", *(argv + 1));
+		exit(98);
+	}
+
+	close_source = close(source);
+
+	if (close_source < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", source), exit(100);
+
+	close_new = close(new);
+
+	if (close_new < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", new), exit(100);
+
+	return (0);
+}

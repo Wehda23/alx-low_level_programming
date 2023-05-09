85e3e13d32f036eab77df717341b05de062ce9bb
Waheed Khaled <waheedkhaled95@gmail.com>
2023-05-09 01:00:17 +0000

    Edite to pass betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 
 	while (text_content[length] != '\0')
-			length++;
+		length++;
 
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 

4549405194d797fdb8de927b395ea4508db7acc2
Waheed Khaled <waheedkhaled95@gmail.com>
2023-05-09 03:58:16 +0300

    updated 2nd file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 
-	write(file, text_content, i);
+	write(file, text_content, length);
 
 	return (1);
 }

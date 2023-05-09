df07e372303666db9a33f744db63d9c48d59f71c
Waheed Khaled <waheedkhaled95@gmail.com>
2023-05-09 04:03:23 +0300

    edit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,23 +9,28 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file, length;
+	int file, length, w;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
+	if (text_content != NULL)
+	{	
+		while (text_content[length] != '\0')
+			length++;
 
-	while (text_content[length] != '\0')
-		length++;
+		file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+		if (file == -1)
+			return (-1);
 
-	if (file == -1)
-		return (-1);
+		w = write(file, text_content, length);
 
-	write(file, text_content, length);
+		if (w == -1)
+			return (-1);
+	}
 
+	close(file);
+	
 	return (1);
 }

91bd87748a0f82814491f6ff52bf0f85a441e8ec
Waheed Khaled <waheedkhaled95@gmail.com>
2023-05-09 02:19:00 +0000

    done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,17 +18,13 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	source = open(*(argv + 1), O_RDONLY);
-    
 	if (source == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", *(argv + 1));
 		exit(98);
 	}
-
 	new = open(*(argv + 2), O_TRUNC | O_CREAT | O_WRONLY, 0664);
-
 	while ((r = read(source, buffer, 1024)) > 0)
 	{
 		if (new == -1 || (write(new, buffer, r) != r))
@@ -37,22 +33,16 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-
 	if (r < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", *(argv + 1));
 		exit(98);
 	}
-
 	close_source = close(source);
-
 	if (close_source < 0)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", source), exit(100);
-
 	close_new = close(new);
-
 	if (close_new < 0)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", new), exit(100);
-
 	return (0);
 }

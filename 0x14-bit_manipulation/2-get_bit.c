0219f6c651a5d74b699d0b097989874dd6ac01af
Waheed Khaled <waheedkhaled95@gmail.com>
2023-10-27 03:28:36 +0200

    Edited file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,37 +8,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	char s[1024], reversed_s[1024];
-	int i = 0, number = n, modulus = 0, index_ = 0, j = 0, r = 0;
 
-	if (number == 0)
-	{
-		return (0);
-	}
-
-	if (index >= sizeof(n) * 8)
+	if (index > 64)
 		return (-1);
-
-	for (index_ = 0; number > 0; index_++)
-	{
-		modulus = number % 2;
-		number /= 2;
-		if (modulus == 1)
-			s[index_] = '1';
-		else
-			s[index_] = '0';
-
-		i = index_;
-	}
-
-	s[i] = '\0';
-
-	for (j = i; j >= 0; j--)
-	{
-		reversed_s[r] = s[j];
-		r++;
-	}
-	reversed_s[r] = '\0';
-
-	return (reversed_s[index] - '0');
+	return ((n >> index) & 0x1);
 }

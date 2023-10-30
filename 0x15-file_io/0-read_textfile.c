fcb5f4902db6607a40a9642ed3f04aaed8b3d305
Waheed Khaled <waheedkhaled95@gmail.com>
2023-10-31 00:41:21 +0200

    new Project

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+
+
+/**
+ * read_textfile - Function that reads a file and prints the content of file.
+ * @filename: File name
+ * @letters: File content
+ * Return: content of file. 
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+    return 0;
+}

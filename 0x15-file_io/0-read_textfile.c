a5f03a015af7819a9e20797d99a2e560c8cd9a1e
Waheed Khaled <waheedkhaled95@gmail.com>
2023-10-31 03:54:42 +0200

    updates

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,6 +11,43 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	ssize_t bytesRead = fread(buffer, 1, letters, file);
+	FILE *file = fopen(filename, "r");
 
-    return 0;
+	if (filename == NULL) {
+		return 0;
+	}
+
+
+
+	if (file == NULL) {
+		return 0;
+	}
+
+
+	char *buffer = (char *)malloc(letters);
+	if (buffer == NULL) {
+		fclose(file);
+		return 0;
+	}
+
+	if (bytesRead <= 0) {
+		fclose(file);
+		free(buffer);
+		return 0;
+	}
+
+
+	ssize_t bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+
+	if (bytesWritten <= 0 || (size_t)bytesWritten < bytesRead) {
+		fclose(file);
+		free(buffer);
+		return 0;
+	}
+
+
+	fclose(file);
+	free(buffer);
+	return bytesRead;
 }

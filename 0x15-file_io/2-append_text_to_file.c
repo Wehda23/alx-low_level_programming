9a55e2ba0fd9d5c3f70a24ae658b1237c3e67c6d
Waheed Khaled <waheedkhaled95@gmail.com>
2023-10-31 02:28:25 +0000

    last check

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,38 +8,38 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int w, file, length;
+	int w, file, length;
 
-    if (filename == NULL)
-    {
-        return (-1);
-    }
+	if (filename == NULL)
+	{
+		return (-1);
+	}
 
-    file = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0600);
+	file = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0600);
 
-    if (file == -1)
-    {
-        return (-1);
-    }
+	if (file == -1)
+	{
+		return (-1);
+	}
 
-    if (text_content)
-    {
-        length = 0;
-        while (text_content[length]) 
-        {
-            length++;
-        }
+	if (text_content)
+	{
+		length = 0;
+		while (text_content[length])
+		{
+			length++;
+		}
 
-        w = write(file, text_content, length);
+		w = write(file, text_content, length);
 
-        if (w == -1)
-        {
-            close(file);
-            return (-1);
-        }
-    }
+		if (w == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
 
-    close(file);
+	close(file);
 
-    return (1);
+	return (1);
 }

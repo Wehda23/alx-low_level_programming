f5d748912cb966e631fb839d8618a1cd40db2969
Waheed Khaled <waheedkhaled95@gmail.com>
2023-10-31 02:24:15 +0000

    improved for betty check

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,29 +11,29 @@ int create_file(const char *filename, char *text_content)
 {
 	int folder, length, w;
 
-	if (filename == NULL) 
+	if (filename == NULL)
 	{
 		return (-1);
 	}
 
 	folder = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-	if (folder == -1) 
+	if (folder == -1)
 	{
 		return (-1);
 	}
 
-	if (text_content != NULL) 
+	if (text_content != NULL)
 	{
 		length = 0;
-		while (text_content[length]) 
+		while (text_content[length])
 		{
 			length++;
 		}
 
 		w = write(folder, text_content, length);
 
-		if (w == -1) 
+		if (w == -1)
 		{
 			close(folder);
 			return (-1);

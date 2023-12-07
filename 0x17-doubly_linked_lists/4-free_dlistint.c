1f7e6ed978d6e42050d7f1630512737936441102
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-07 18:35:13 +0000

    task 4 done

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,6 +1,9 @@
 #include "lists.h"
 
-
+/**
+ * free_dlistint - function that frees linked list
+ * @head: pointer to start of a linked list
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *current = head;

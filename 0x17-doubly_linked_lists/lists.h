4f7819583eeb3d48087a271f305019001f61d8e6
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-07 18:33:49 +0000

    task 4 done

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -27,4 +27,6 @@ size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 /* 3-add_dnodeint_end.c */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+/* 4-free_dlistint.c */
+void free_dlistint(dlistint_t *head);
 #endif

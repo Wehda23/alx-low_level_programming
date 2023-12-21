e1422feb35ebc8e88fe1c7a283d1288fd60debff
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-22 01:40:09 +0200

    test first task

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - is a function used to create a hash table
+ * @size: Size of the hashtable
+ * Return: hash_table_t* pointer to a hashtable or NULL in case of failure
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *new = malloc(sizeof(hash_table_t));
+
+    if (!new)
+    {
+        return (NULL);
+    }
+    new->size = size;
+    return (new);
+}

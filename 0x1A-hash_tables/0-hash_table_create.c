028f474eb88de55e8fabdcf1eec45fae1bd9c41b
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-21 23:43:52 +0000

    Testing function

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,12 +7,12 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new = malloc(sizeof(hash_table_t));
+	hash_table_t *new = malloc(sizeof(hash_table_t));
 
-    if (!new)
-    {
-        return (NULL);
-    }
-    new->size = size;
-    return (new);
+	if (!new)
+	{
+		return (NULL);
+	}
+	new->size = size;
+	return (new);
 }

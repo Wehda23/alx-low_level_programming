8cfab392ccbe24e07551824efb492c105eeb8d3c
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-22 21:55:20 +0000

    task 3 done

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hast_tables.h"
+
+
+/**
+ * key_index - function that gives me index of a key
+ * @key: value of the key.
+ * @size: size of the array of hash table
+ * Return: Unsinged integer which represents the index of the key.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}

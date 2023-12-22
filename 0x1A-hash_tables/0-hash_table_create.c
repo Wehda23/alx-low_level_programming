4d7dd9c627453d0f9066582ecd1b5ab6ef499106
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-22 02:42:37 +0200

    Test

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,12 +8,22 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new = malloc(sizeof(hash_table_t));
+    int index = 0;
 
 	if (!new)
 	{
 		return (NULL);
 	}
 	new->size = size;
-	new->array = NULL;
+	new->array = malloc(sizeof(hash_node_t) * size);
+
+    if(!new->array)
+    {
+        return (NULL);
+    }
+
+    for(index = 0; index < size; index++)
+        new->array[index] = NULL;
+        
 	return (new);
 }

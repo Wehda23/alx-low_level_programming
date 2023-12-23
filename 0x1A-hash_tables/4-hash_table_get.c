c36bc8231aee06ca12a50588ff465ce644dd4f8c
Waheed Khaled <waheedkhaled95@gmail.com>
2023-12-23 01:11:57 +0000

    task 5 done

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_get - Retrieve a value associated with a key
+ * @ht: Hash table
+ * @key: Key name.
+ * Return: Value inside that key.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	char *value;
+	int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((unsigned char *) key, ht->size);
+
+	if (ht->array[index] == NULL)
+		return (NULL);
+
+	value = strdup(ht->array[index]->value);
+
+	return (value);
+}

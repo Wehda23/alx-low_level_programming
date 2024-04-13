5c7bd5be3fd32946d207ef24465cc55e48ef82fb
Waheed Khaled <waheedkhaled95@gmail.com>
2024-04-13 13:15:28 +0200

    Created a print array function

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -19,6 +19,8 @@ int binary_search(int *array, size_t size, int value)
 		/* While loop for binary search */
 		while (left <= right)
 		{
+            /* Print the array */
+			print_array(array, left, right);
 			/* Calculate the median */
 			median = left + (right - left) / 2;
 			/* Check if the median is equal to the value */
@@ -39,4 +41,27 @@ int binary_search(int *array, size_t size, int value)
 	}
 
 	return (-1);
+}
+
+
+/**
+ * print_array - prints an array from specific start to end
+ * @array: Array that contains integers.
+ * @start: Start index at which to print the array.
+ * @end: End index at which to print the array.
+ */
+void print_array(int *array, int start, int end)
+{
+    int index = start;
+
+    printf("Searching in array: ");
+    while (index <= end)
+    {
+        printf("%d", array[index]);
+        if (index != end)
+            printf(", ");
+
+        index++;
+    }
+    printf("\n");
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,5 +8,6 @@
 int linear_search(int *array, size_t size, int value);
 /* 1-binary.c */
 int binary_search(int *array, size_t size, int value);
+void print_array(int *array, int start, int end);
 
 #endif /* End of search algorithms header */

fffd77fd452b9b6f5d4502966d124fe949a38576
Waheed Khaled <waheedkhaled95@gmail.com>
2024-04-13 11:08:33 +0000

    1-binary.c pass betty checkers

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,32 +11,32 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    int left = 0, right = (int) size - 1, median = 0;
+	int left = 0, right = (int) size - 1, median = 0;
 
-    /* Check Array is Null*/
-    if (array != NULL)
-    {
-        /* While loop for binary search */
-        while (left <= right)
-        {
-            // Calculate the median
-            median = left + (right - left) / 2;
-            /* Check if the median is equal to the value */
-            if (array[median] == value)
-            {
-                    return (median);
-            }
-            /* In Case median is larger than the left */
-            else if (array[median] > value)
-            {
-                right = median - 1;
-            }
-            else
-            {
-                left = median + 1;
-            }
-        };
-    }
+	/* Check Array is Null*/
+	if (array != NULL)
+	{
+		/* While loop for binary search */
+		while (left <= right)
+		{
+			/* Calculate the median */
+			median = left + (right - left) / 2;
+			/* Check if the median is equal to the value */
+			if (array[median] == value)
+			{
+				return (median);
+			}
+			/* In Case median is larger than the left */
+			else if (array[median] > value)
+			{
+				right = median - 1;
+			}
+			else
+			{
+				left = median + 1;
+			}
+		};
+	}
 
-    return (-1);
+	return (-1);
 }

072fb524c5e00f923f7ab2cbec3f03ed0e1cb91c
Waheed Khaled <waheedkhaled95@gmail.com>
2024-04-14 04:37:25 +0200

    Working

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,18 @@
+#include "search_algos.h"
+
+
+/**
+ * jump_search - Searches for a value in an array of integers\
+ * Using Jump Search.
+ * @array: Pointer to the first element of the array to search in.
+ * @size: Number of elements in array.
+ * @value: Value to search for.
+ * Return: First index where value is located
+ */
+int jump_search(int *array, size_t size, int value)
+{
+    int square_root = (int) sqrt((double) size);
+
+    printf("Square root: %d\n", square_root);
+    return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,11 +3,14 @@
 
 /* Includes */
 #include <stdio.h>
+#include <math.h>
 
 /* 0-linear.c */
 int linear_search(int *array, size_t size, int value);
 /* 1-binary.c */
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, int start, int end);
+/* 100-jump.c */
+int jump_search(int *array, size_t size, int value);
 
 #endif /* End of search algorithms header */

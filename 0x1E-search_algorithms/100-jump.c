e55361df3a1b19724647a17e51d04fddb1e8da9a
Waheed Khaled <waheedkhaled95@gmail.com>
2024-04-14 23:04:15 +0000

    test

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -11,36 +11,36 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-    int square_root = (int) sqrt((double) size);
-    int lower = 0, lead = 0;
-    int index = 0;
+	int square_root = (int) sqrt((double) size);
+	int lower = 0, lead = 0;
+	int index = 0;
 
-    if (array != NULL || size == 0)
-    {
-        /*First while loop */
-        do {
-            printf("Value checked array[%d] = [%d]\n", lead, array[lead]);
+	if (array != NULL || size == 0)
+	{
+		/*First while loop */
+		do {
+			printf("Value checked array[%d] = [%d]\n", lead, array[lead]);
 
-            lower = lead;
-            lead += square_root;
-            if (lead > (int) size - 1)
-                lead = size;
-        } while (lead < (int) size && array[lead] < value);
-        /*Assign Index */
-        index = lower;
-        printf("Value found between indexes [%d] and [%d]\n", lower, lead);
-        /* Normal Loop */
-        while (index <= lead)
-        {
-            if (index == lead && array[lead] != value)
-                break;
-            printf("Value checked array[%d] = [%d]\n", index, array[index]);
-            /* Check if index == value */
-            if (array[index] == value)
-                return (index);
-            /* Incerement index */
-            index++;
-        }
-    }
-    return (-1);
+			lower = lead;
+			lead += square_root;
+			if (lead > (int) size - 1)
+				lead = size;
+		} while (lead < (int) size && array[lead] < value);
+		/*Assign Index */
+		index = lower;
+		printf("Value found between indexes [%d] and [%d]\n", lower, lead);
+		/* Normal Loop */
+		while (index <= lead)
+		{
+			if (index == lead && array[lead] != value)
+				break;
+			printf("Value checked array[%d] = [%d]\n", index, array[index]);
+			/* Check if index == value */
+			if (array[index] == value)
+				return (index);
+			/* Incerement index */
+			index++;
+		}
+	}
+	return (-1);
 }

77f20e0f4b035dc6b051179baac57c0170ccbce1
Waheed Khaled <waheedkhaled95@gmail.com>
2024-04-15 01:22:50 +0000

    Done

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -11,30 +11,30 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-    int low = 0, high = (int) size - 1;
-    int probe = 0;
+	int low = 0, high = (int) size - 1;
+	int probe = 0;
 
-    if (array != NULL)
-    {
-        do {
-            probe = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
-            printf("Value checked array[%d]", probe);
+	if (array != NULL)
+	{
+		do {
+			probe = low + (((double)(high - low) / (array[high] - array[low])) *					(value - array[low]));
+			printf("Value checked array[%d]", probe);
 
-            if (probe >= (int) size)
-            {
-                printf(" is out of range\n");
-                break;
-            }
-            else
-                printf(" = [%d]\n", array[probe]);
+			if (probe >= (int) size)
+			{
+				printf(" is out of range\n");
+				break;
+			}
+			else
+				printf(" = [%d]\n", array[probe]);
 
-            if (array[probe] == value)
-                return (probe);
-            else if (array[probe] < value)
-                low = probe + 1;
-            else
-                high = probe - 1;
-        } while (low <= high && value >= array[low] && value <= array[high]);
-    }
+			if (array[probe] == value)
+				return (probe);
+			else if (array[probe] < value)
+				low = probe + 1;
+			else
+				high = probe - 1;
+		} while (low <= high && value >= array[low] && value <= array[high]);
+	}
 	return (-1);
 }
